24bef43a8f14b5e7afa05dc0ba5e883b20a8b219
Otavio Serra <nb27441@novabase.pt>
2021-06-04 18:12:09 +0100

    removed the try catch from the buy controller

--- src/main/java/com/training/springbootbuyitem/controller/BuyController.java
+++ src/main/java/com/training/springbootbuyitem/controller/BuyController.java
@@ -1,12 +1,12 @@
 package com.training.springbootbuyitem.controller;
 
 import com.training.springbootbuyitem.entity.model.Item;
-import com.training.springbootbuyitem.entity.model.User;
 import com.training.springbootbuyitem.entity.request.CreateItemRequestDto;
-import com.training.springbootbuyitem.entity.request.CreateUserRequestDto;
 import com.training.springbootbuyitem.entity.request.DispatchItemRequestDto;
 import com.training.springbootbuyitem.entity.request.RestockItemRequestDto;
-import com.training.springbootbuyitem.entity.response.*;
+import com.training.springbootbuyitem.entity.response.CreateItemResponseDto;
+import com.training.springbootbuyitem.entity.response.GetItemResponseDto;
+import com.training.springbootbuyitem.entity.response.UpdateItemResponseDto;
 import com.training.springbootbuyitem.service.ItemService;
 import com.training.springbootbuyitem.service.UserService;
 import com.training.springbootbuyitem.utils.annotation.ServiceOperation;
@@ -16,15 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -58,11 +50,7 @@ public class BuyController implements IBuyController {
 	@PostMapping("/createItem")
 	@ServiceOperation("createItem")
 	public ResponseEntity<CreateItemResponseDto> createItem(@RequestBody @Valid CreateItemRequestDto request) {
-		try {
-			return new ResponseEntity<>(mapper.map(itemService.save(mapper.map(request, Item.class)), CreateItemResponseDto.class), HttpStatus.CREATED);
-		} catch (Exception e) {
-			throw new
-		}
+		return new ResponseEntity<>(mapper.map(itemService.save(mapper.map(request, Item.class)), CreateItemResponseDto.class), HttpStatus.CREATED);
 	}
 
 
@@ -70,8 +58,8 @@ public class BuyController implements IBuyController {
 	@GetMapping("/{id}")
 	@ServiceOperation("getItem")
 	public ResponseEntity<GetItemResponseDto> getItem(@PathVariable("id") Long id) {
-			log.info("It's working!!!");
-			return new ResponseEntity<>(mapper.map(itemService.get(id), GetItemResponseDto.class), HttpStatus.OK);
+		log.info("It's working!!!");
+		return new ResponseEntity<>(mapper.map(itemService.get(id), GetItemResponseDto.class), HttpStatus.OK);
 	}
 
 	@Override
