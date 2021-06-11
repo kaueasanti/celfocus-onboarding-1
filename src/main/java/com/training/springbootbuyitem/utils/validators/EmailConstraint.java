03059d2e1edbc1e309484ac485a374a7a0948667
Otavio Serra <nb27441@novabase.pt>
2021-06-11 20:02:23 +0100

    Criei custom validations para email e password.

---
+++ src/main/java/com/training/springbootbuyitem/utils/validators/EmailConstraint.java
@@ -0,0 +1,14 @@
+package com.training.springbootbuyitem.utils.validators;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = EmailValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EmailConstraint {
+    String message() default "Invalid email";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
